d4c0cdd4de56643a81f683f2a34bc8eb871965e3
Gustavo <jg_barreto@hotmail.com>
2017-04-30 11:02:33 -0400

    added valueChanges with observables

--- src/app/exercises.component.ts
+++ src/app/exercises.component.ts
@@ -1,9 +1,12 @@
-import {Component} from '@angular/core';
-import {FormGroup, FormControl, FormBuilder} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
 
 @Component({
-    selector:'search-form',
-    template:`
+    selector: 'search-form',
+    template: `
     <form (submit)='search()' [formGroup]='form'>
         <fieldset>
             <label class='control-label' for='searchText' >Search artist</label>
@@ -19,11 +22,19 @@ import {FormGroup, FormControl, FormBuilder} from '@angular/forms';
     `
 
 })
-export class ExercisesComponent{
-form:FormGroup;
-constructor(private fb:FormBuilder){
-    this.form =fb.group({
-        searchText:''
-    })
-}
+export class ExercisesComponent {
+    form: FormGroup;
+    constructor(private fb: FormBuilder) {
+        this.form = fb.group({
+            searchText: []
+        });
+       var search= this.form.get('searchText');
+
+            search.valueChanges
+            .debounceTime(400)
+            .map(str => (<string>str).replace(' ', '-'))
+            .subscribe(x => console.log(x))
+            
+            ;
+    }
 }
