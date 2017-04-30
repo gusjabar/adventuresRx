33a5be4c11fa73ca3f099e92a8cf75a0d3f8f573
Gustavo <jg_barreto@hotmail.com>
2017-04-30 11:28:42 -0400

    creating observables from arrays

--- src/app/exercises.component.ts
+++ src/app/exercises.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
-import { Observable } from 'rxjs/Rx';
+import * as Rx from 'rxjs/Rx';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/map';
+
 
 @Component({
     selector: 'search-form',
@@ -22,19 +23,44 @@ import 'rxjs/add/operator/map';
     `
 
 })
-export class ExercisesComponent {
+export class ExercisesComponent implements OnInit {
     form: FormGroup;
     constructor(private fb: FormBuilder) {
         this.form = fb.group({
             searchText: []
         });
-       var search= this.form.get('searchText');
+        var search = this.form.get('searchText');
 
-            search.valueChanges
+        search.valueChanges
             .debounceTime(400)
             .map(str => (<string>str).replace(' ', '-'))
             .subscribe(x => console.log(x))
-            
+
             ;
+    }
+    ngOnInit() {
+        var startDates = [];
+        var startDate = new Date();
+        //Create an array of the travel start dates
+        //with the 2-day window
+
+        for (var day = 1; day <= 5; day++) {
+            var date = new Date(
+                startDate.getFullYear(),
+                startDate.getMonth(),
+                startDate.getDate() + day
+            );
+            startDates.push(date);
+        }
+        //convert this startDates[] array to Observable.
+        var observable = Rx.Observable
+            .from(startDates)
+            .map(date => {
+                console.log('Getting deals for date ' + date);
+                return [1, 2, 3]
+            })
+            .subscribe(x => console.log(x));
+
+
     }
 }
